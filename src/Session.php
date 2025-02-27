dc6934999bd9f234338b77a4bdd7c1fdc8b1fba2
Marijn Ophorst <marijn@sensimedia.nl>
2025-02-27 19:17:57 +0100

    add type safety

--- src/Session.php
+++ src/Session.php
@@ -62,7 +62,7 @@ class Session implements SessionHandlerInterface
      * @param array $args The arguments to pass to $method.
      * @return mixed Whatever $method returned.
      */
-    private function walk(string $method, ?int $highProbability = null, array $args = [])
+    private function walk(string $method, ?int $highProbability = null, array $args = []) : mixed
     {
         $result = false;
         foreach ($this->handlers as $data) {
@@ -86,7 +86,7 @@ class Session implements SessionHandlerInterface
      * @throws Monolyth\Cesession\NoHandlersDefinedException if no handlers were defined
      *  (which would make this whole module less than useful anyway).
      */
-    public function open($save_path, $name) : bool
+    public function open(string $save_path, string $name) : bool
     {
         if (!$this->handlers) {
             throw new NoHandlersDefinedException;
@@ -121,7 +121,7 @@ class Session implements SessionHandlerInterface
      * @param string $id The session ID.
      * @return string The read data.
      */
-    public function read($id) : string
+    public function read(string $id) : string
     {
         if ($session = $this->walk('read', null, [$id])) {
             self::$session = $session;
@@ -137,7 +137,7 @@ class Session implements SessionHandlerInterface
      * @param string $data The serialized session data as passed by PHP.
      * @return boolean True on success, else false.
      */
-    public function write($id, $data) : bool
+    public function write(string $id, string $data) : bool
     {
         return (bool)$this->walk(
             'write',
@@ -152,7 +152,7 @@ class Session implements SessionHandlerInterface
      * @param string $id The session ID.
      * @return boolean True on success, else false.
      */
-    public function destroy($id) : bool
+    public function destroy(string $id) : bool
     {
         // Override with 100 to close every handler.
         return (bool)$this->walk('destroy', 100, [$id]);
@@ -163,10 +163,9 @@ class Session implements SessionHandlerInterface
      *
      * @param integer $maxlifetime The number of seconds stale session data must
      *  be older than to be eligible for garbage collection.
-     * @return boolean True on success, else false.
+     * @return int|false The number of deleted sessions, or `false` on failure.
      */     
-    #[\ReturnTypeWillChange]
-    public function gc($maxlifetime) : bool
+    public function gc(int $maxlifetime) : int|false
     {
         return $this->walk('gc', null, [$maxlifetime]);
     }
